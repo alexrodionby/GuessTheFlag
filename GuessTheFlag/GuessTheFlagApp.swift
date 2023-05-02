a8072e70c679fe07a5afcd1ae3772b02d38a496a
Alexandr Rodionov <alexrodionby@gmail.com>
2023-05-02 21:09:20 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexandr Rodionov on 2.05.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
